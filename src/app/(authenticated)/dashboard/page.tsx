0899592d48afc0cc9986bc20f3dbc185c2ae0b0b
Staci Peabody <stacipeabody@gmail.com>
2026-06-07 06:05:29 +0000

    fix: dashboard jsx syntax error (extra div)

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -213,7 +213,6 @@ export default function Dashboard() {
                   <EmpireConstellation />
                 </aside>
               </div>
-            </div>
           </div>
         </div>
 
