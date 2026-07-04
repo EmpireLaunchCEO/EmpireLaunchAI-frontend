c4222e6892bf4de3b78c8dc53b3ca7ef054540d2
Staci Peabody <stacipeabody@gmail.com>
2026-07-04 16:23:01 +0000

    force PWA cache refresh on next load

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,15 @@
-console.log('Service worker disabled');
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  // Clear all caches
+  caches.keys().then((names) => {
+    names.forEach((name) => caches.delete(name));
+  });
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach(client => client.navigate(client.url));
+    });
+});
